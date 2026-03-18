56f02357e27b7f4e40fe7fbd1925b02125ce51a5
Alexander Maltsev <shura.mv@truelabel.io>
2026-03-18 09:53:51 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
